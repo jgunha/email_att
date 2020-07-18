d895afbfcf5b2ebe5ede5fa8f49fbcfc85ea1dba
Jgunha <jgunha@gmail.com>
2020-07-19 02:31:16 +0900

    get exif and some of part modified

--- main.py
+++ main.py
@@ -4,28 +4,39 @@ import imaplib
 import time
 import requests
 
+from GPSPhoto import gpsphoto
+
+
 #디렉토리 생성
 dir = time.strftime('%Y-%m-%d', time.localtime(time.time()))
-
+temp = time.strftime('%H_%M_%S', time.localtime(time.time()))
 if not(os.path.isdir(dir)):
     os.makedirs(os.path.join(dir))
 
+#csv 파일 생성
+if not(os.path.isfile(os.getcwd()+'\\'+dir+'\\'+'result.csv')):
+    f = open(os.getcwd() + '\\'+dir+'\\'+'result.csv', 'w')
+    f.close()
+
 #파일 다운로드
 def download(url, file_name = None):
-    print(url)
     if not file_name:
         file_name = url.split('/')[-1]
-    '''with open(dir +'/' + file_name, "wb") as file:
+    with open(os.getcwd() + '\\' + dir + '\\' + temp+'.txt' , "wb") as file:
         response = requests.get(url)
-        file.write(response.content)'''
-    response = requests.get(url)
-    print(response.text)
+        file.write(response.content)
+
+    #exif 정보 가져오기
+    #csv 파일 쓰기
 
 #문자열이 인코딩 정보 추출 후, 문자열, 인코딩 얻기
 def find_encoding_info(txt):
     info = email.header.decode_header(txt)
     s, encoding = info[0]
     return s, encoding
+#위도 경도 고도 정보 가져오기
+def get_exif(jpg_file):
+    return gpsphoto.getGPSData(jpg_file)
 
 #gmail imap 세션 설정
 session = imaplib.IMAP4_SSL('imap.gmail.com')
@@ -33,48 +44,55 @@ session = imaplib.IMAP4_SSL('imap.gmail.com')
 #로그인
 session.login('[mail address]','[password]')
 
-#받은편지함 선택
-session.select('Inbox')
 
-#특정 발신 이메일, 안읽은 메일만 검색
-result, data = session.uid('search', None, '(UNSEEN)', '(HEADER From "[certain email address]]")')
+# 계속 동작
+while(1):
+    # 받은편지함 선택
+    session.select('Inbox')
 
-#메일 나누기
-all_email = data[0].split()
+    #특정 발신 이메일, 안읽은 메일만 검색
+    result, data = session.uid('search', None, '(UNSEEN)', '(HEADER From "[certain email address]")')
 
-for mail in all_email:
-    result, data = session.uid('fetch', mail,'(RFC822)')
-    raw_email = data[0][1]
-    raw_email_string = raw_email.decode('utf-8')
-    email_message = email.message_from_string(raw_email_string)
+    #메일 나누기
+    all_email = data[0].split()
+
+    #사진 정보 가져오기
+    info = get_exif('test.jpg')
+    print(info)
+
+    for mail in all_email:
+        result, data = session.uid('fetch', mail,'(RFC822)')
+        raw_email = data[0][1]
+        raw_email_string = raw_email.decode('utf-8')
+        email_message = email.message_from_string(raw_email_string)
 
 
-    #메일정보
-    print('From:', email_message['From'])
-    print('Sender:', email_message['Sender'])
-    print('To:', email_message['To'])
-    print('Date:', email_message['Date'])
+        #메일정보
+        print('From:', email_message['From'])
+        print('Sender:', email_message['Sender'])
+        print('To:', email_message['To'])
+        print('Date:', email_message['Date'])
 
-    subject, encode = find_encoding_info(email_message['Subject'])
-    print('Subject', subject)
+        subject, encode = find_encoding_info(email_message['Subject'])
+        print('Subject', subject)
 
-    message = ''
-    print('[Message]')
+        message = ''
+        print('[Message]')
 
-    if email_message.is_multipart():
-        for part in email_message.get_payload():
-            if part.get_content_type()=='text/plain':
-                bytes = part.get_payload(decode=True)
-                encode = part.get_content_charset()
-                message = message + str(bytes, encode)
-    else:
-        if email_message.get_content_type() =='text/plain':
-            bytes = email_message.get_payload(decode=True)
-            encode = email_message.get_content_charset()
-            message = str(bytes, encode)
+        if email_message.is_multipart():
+            for part in email_message.get_payload():
+                if part.get_content_type()=='text/plain':
+                    bytes = part.get_payload(decode=True)
+                    encode = part.get_content_charset()
+                    message = message + str(bytes, encode)
+        else:
+            if email_message.get_content_type() =='text/plain':
+                bytes = email_message.get_payload(decode=True)
+                encode = email_message.get_content_charset()
+                message = str(bytes, encode)
 
-    print(message)
-    #download(message)
+        print(message)
+        download(message)
 
 session.close()
 session.logout()
