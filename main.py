f156dccead6e0aacdd20e6685e76a2bb5ff4dd55
Jgunha <jgunha@gmail.com>
2020-08-05 02:11:44 +0900

    all complete

--- main.py
+++ main.py
@@ -1,31 +1,39 @@
 import os
 import email
 import imaplib
-import time
 import requests
 from GPSPhoto import gpsphoto
 import csv
 import hashlib
 import urllib
 import datetime
+import re
 
 #디렉토리 생성
-dir = time.strftime('%Y-%m-%d', time.localtime(time.time()))
-temp = time.strftime('%H_%M_%S', time.localtime(time.time()))
-if not(os.path.isdir(dir)):
-    os.makedirs(os.path.join(dir))
+def make_dir(convert_date):
+    dir_date = str(datetime.datetime.strftime(convert_date, '%Y-%m-%d'))
 
-#csv 파일 생성
-if not(os.path.isfile(os.getcwd()+'\\'+dir+'\\'+'result.csv')):
-    f = open(os.getcwd() + '\\'+dir+'\\'+'result.csv', 'w', encoding='utf-8',newline='')
-    wr = csv.writer(f)
-    wr.writerow(['Number','Data', 'Shortened URL', 'Full URL', 'FileName', 'Latitiude', 'Longitude', 'Altitude', 'MD5', 'SHA1'])
-    f.close()
+    if not(os.path.isdir(dir_date)):
+        os.makedirs(os.path.join(dir_date))
+    #csv 파일 생성
+    if not(os.path.isfile(os.getcwd()+'\\'+dir_date+'\\'+'result.csv')):
+        f = open(os.getcwd() + '\\'+dir_date+'\\'+'result.csv', 'w', encoding='utf-8', newline='')
+        wr = csv.writer(f)
+        wr.writerow(['Number','Data', 'Shortened URL', 'Full URL', 'FileName', 'Latitiude', 'Longitude', 'Altitude', 'MD5', 'SHA1'])
+        f.close()
+    return dir_date
 
+#정규표현식을 통한 url 추출
 def find_url(message):
-    url = 'https://' + message.split('/')[-2] +'/'+ message.split('/')[-1].split('.')[0]
+    p = re.compile("https://bit.ly/[a-zA-Z0-9]*")
+    url = p.search(message).group()
     return url
-#파일 다운로드
+
+#위도 경도 고도 정보 가져오기
+def get_exif(jpg_file):
+    return gpsphoto.getGPSData(jpg_file)
+
+#파일 다운로드 & exif 정보 추출
 def download(url):
     r = requests.get(url, verify=False)
     filename = urllib.parse.unquote(r.url.split('/')[-1])
@@ -34,10 +42,14 @@ def download(url):
     info = get_exif(os.getcwd()+'\\'+dir+'\\'+filename)
     if 'Latitude' in info.keys():
         lat = info['Latitude']
+        if lat < 0:
+            lat = -lat
     else:
         lat = 'N/A'
     if 'Longitude' in info.keys():
         long = info['Longitude']
+        if long < 0:
+            long = -long
     else:
         long = 'N/A'
     if 'Altitude' in info.keys():
@@ -45,18 +57,16 @@ def download(url):
     else:
         alt = 'N/A'
     return full_url, lat, long, alt, filename
-    #exif 정보 가져오기
-    #csv 파일 쓰기
+
 
 #문자열이 인코딩 정보 추출 후, 문자열, 인코딩 얻기
 def find_encoding_info(txt):
     info = email.header.decode_header(txt)
     s, encoding = info[0]
     return s, encoding
-#위도 경도 고도 정보 가져오기
-def get_exif(jpg_file):
-    return gpsphoto.getGPSData(jpg_file)
 
+
+#해시값 가져오기
 def get_hash(file_name):
     BLOCKSIZE = 1024
     hasher = hashlib.md5()
@@ -71,26 +81,35 @@ def get_hash(file_name):
     hash_md5 = hasher.hexdigest()
     hash_sha1 = hasher2.hexdigest()
     return hash_md5, hash_sha1
+
 #csv 작성
-def write_csv(url, date):
+def write_csv(url, date, dir):
     full_url, lat, long, alt, file_name = download(url)
     #csv 작성
     f = open(os.getcwd() + '\\'+dir+'\\'+'result.csv', 'r')
     rd = csv.reader(f)
-    row_count = sum(1 for row in rd)
+    for url_data in rd:
+        #중복 제거
+        if url == url_data[2]:
+            return
+        else:
+            if url_data[0] == 'Number':
+                row_count = 1
+            else:
+                row_count = int(url_data[0])+1
+
     f.close()
     f2 = open(os.getcwd() + '\\'+dir+'\\'+'result.csv', 'a', newline='')
     wr = csv.writer(f2)
     mail_date = datetime.datetime.strftime(date, '%Y-%m-%d %H:%M')
     hash_md5, hash_sha1 = get_hash(file_name)
     wr.writerow([row_count,mail_date ,url, full_url , file_name, lat, long, alt, hash_md5, hash_sha1])
-    pass
 
 #gmail imap 세션 설정
 session = imaplib.IMAP4_SSL('imap.gmail.com')
 
 #로그인
-session.login('[mail address]','[password]')
+session.login('메일 주소','비밀 번호')
 
 
 # 계속 동작
@@ -99,7 +118,7 @@ while(1):
     session.select('Inbox')
 
     #특정 발신 이메일, 안읽은 메일만 검색
-    result, data = session.uid('search', None, '(UNSEEN)', '(HEADER From "[certain email address]")')
+    result, data = session.uid('search', None, '(UNSEEN)', '(HEADER From "certain email address")')
 
     #메일 나누기
     all_email = data[0].split()
@@ -111,18 +130,9 @@ while(1):
         raw_email_string = raw_email.decode('utf-8')
         email_message = email.message_from_string(raw_email_string)
 
-
-        #메일정보
-        print('From:', email_message['From'])
-        print('Sender:', email_message['Sender'])
-        print('To:', email_message['To'])
-        print('Date:', email_message['Date'])
-
         subject, encode = find_encoding_info(email_message['Subject'])
-        print('Subject', subject)
 
         message = ''
-        print('[Message]')
 
         if email_message.is_multipart():
             for part in email_message.get_payload():
@@ -130,17 +140,19 @@ while(1):
                     bytes = part.get_payload(decode=True)
                     encode = part.get_content_charset()
                     message = message + str(bytes, encode)
+                    convert_date = datetime.datetime.strptime(email_message['Date'], '%a, %d %b %Y %X %z')
+                    dir = make_dir(convert_date)
+                    url = find_url(message)
+                    write_csv(url, convert_date, dir)
         else:
             if email_message.get_content_type() =='text/plain':
                 bytes = email_message.get_payload(decode=True)
                 encode = email_message.get_content_charset()
                 message = str(bytes, encode)
-
-        convert_date = datetime.datetime.strptime(email_message['Date'], '%a, %d %b %Y %X %z')
-        print(convert_date)
-        url = find_url(message)
-        print(url)
-        write_csv(url,convert_date)
+            convert_date = datetime.datetime.strptime(email_message['Date'], '%a, %d %b %Y %X %z')
+            dir = make_dir(convert_date)
+            url = find_url(message)
+            write_csv(url, convert_date, dir)
 
 session.close()
 session.logout()
